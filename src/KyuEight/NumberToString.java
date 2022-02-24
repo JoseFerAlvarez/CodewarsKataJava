f99a17a7dd171f10900d48521f8756f02ca3b8d8
JoseFerAlvarez <joseferalvarezromero@gmail.com>
2022-02-24 10:20:32 +0100

    Commit con el kata NumberToString

---
+++ src/KyuEight/NumberToString.java
@@ -0,0 +1,23 @@
+package KyuEight;
+
+import java.util.Scanner;
+
+/*@author JoseFerAlvarez
+
+--------------- Instructions -------------
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+---------------------------------------------*/
+
+public class NumberToString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(numberToString(num));
+    }
+    
+    //The code below is the solution
+    public static String numberToString(int num){
+        return String.valueOf(num);
+    }
+}
